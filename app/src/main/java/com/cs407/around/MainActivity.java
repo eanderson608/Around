7ffdf49735270ad41d009cc2b2984b1db495160f
Eric Anderson <eanderson608@gmail.com>
2016-03-07 19:26:48 -0600

    changed theme to remove ActionBar and halfway implemented facebook login on MainActivity by following this tutorial:
    http://code.tutsplus.com/tutorials/quick-tip-add-facebook-login-to-your-android-app--cms-23837

--- app/src/main/java/com/cs407/around/MainActivity.java
+++ app/src/main/java/com/cs407/around/MainActivity.java
@@ -1,13 +1,57 @@
 package com.cs407.around;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView fbInfo;
+    private LoginButton fbLoginButton;
+
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
         setContentView(R.layout.activity_main);
+        fbInfo = (TextView) findViewById(R.id.fb_info_textview);
+        fbLoginButton = (LoginButton) findViewById(R.id.fb_login_button);
+
+        fbLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                fbInfo.setText("User ID: " + loginResult.getAccessToken().getUserId() + "\n" +
+                                "Auth Token: " + loginResult.getAccessToken().getToken() + "\n" +
+                                "loginResult.toString(): " + loginResult.toString());
+
+            }
+
+            @Override
+            public void onCancel() {
+                fbInfo.setText("login canceled");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                fbInfo.setText("login error");
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
