1cedd11aaafd671171cb9d4f68e405e2161322b4
Eric Anderson <eanderson608@gmail.com>
2016-03-12 00:32:47 -0600

    created User class

---
+++ app/src/main/java/com/cs407/around/User.java
@@ -0,0 +1,53 @@
+package com.cs407.around;
+
+
+import java.util.ArrayList;
+
+public class User {
+
+    private String id;
+    private String name;
+    private String firstName;
+    private String lastName;
+    private String authToken;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAuthToken() {
+        return authToken;
+    }
+
+    public void setAuthToken(String authToken) {
+        this.authToken = authToken;
+    }
+}
