47edf920c50e5af990a23f814f831286562da520
mskrade <michael@skrade.com>
2016-05-05 21:04:57 -0500

    final photo edit

--- app/src/main/java/com/cs407/around/PhotoReviewActivity.java
+++ app/src/main/java/com/cs407/around/PhotoReviewActivity.java
@@ -1,5 +1,8 @@
 package com.cs407.around;
 
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
@@ -8,22 +11,29 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.squareup.picasso.MemoryPolicy;
 import com.squareup.picasso.Picasso;
-import com.squareup.picasso.Target;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -41,12 +51,24 @@ public class PhotoReviewActivity extends AppCompatActivity {
 
     ImageView imageView;
     Button button;
+    Button closeButton;
+    Button textButton;
+    Button paintButton;
     Photo photo;
     User me;
     PreferencesHelper prefs;
     Bitmap bitmap;
+    Bitmap textBitmap;
+    Bitmap combined;
     Canvas canvas;
-    Paint paint;
+    RelativeLayout layout;
+    EditText textBox;
+    boolean alreadyTextBox;
+
+    //boolean paintMode;
+
+    //DrawingView drawView;
+
 
 
     @Override
@@ -55,6 +77,13 @@ public class PhotoReviewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_photo_review);
 
         button = (Button) findViewById(R.id.save_photo_button);
+        closeButton = (Button) findViewById(R.id.close_button);
+        textButton = (Button) findViewById(R.id.text_button);
+        paintButton = (Button) findViewById(R.id.paint_button);
+        layout = (RelativeLayout) findViewById(R.id.relativeLayout);
+        alreadyTextBox = false;
+        //paintMode = false;
+        EditText editText = new EditText(this);
         prefs = new PreferencesHelper(this);
 
         // retrieve photo and place in imageView
@@ -64,7 +93,7 @@ public class PhotoReviewActivity extends AppCompatActivity {
         bitmap = bitmap.copy(bitmap.getConfig(), true);
 
         imageView = (ImageView) findViewById(R.id.photo_review_imageview);
-        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
 
         String camera = prefs.getPreferences("camera");
         if (camera.equals("1")) {
@@ -74,12 +103,6 @@ public class PhotoReviewActivity extends AppCompatActivity {
         }
 
         imageView.setImageBitmap(bitmap);
-
-
-
-
-
-
 
         // save image back to temp storage
         FileOutputStream out = null;
@@ -100,14 +123,12 @@ public class PhotoReviewActivity extends AppCompatActivity {
         }
 
 
-
         // retrieve photo location from intent
         double[] location = getIntent().getDoubleArrayExtra("location");
 
         //Retrieve current user
         Gson gson = new Gson();
         String json = prefs.getPreferences("me");
-        Log.d("JSON ME", json);
         me = gson.fromJson(json, User.class);
 
         //Create Photo object
@@ -116,16 +137,133 @@ public class PhotoReviewActivity extends AppCompatActivity {
         photo.setLocation(location);
         Log.d("PHOTO CREATED", photo.toString());
 
+        imageView.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                if (alreadyTextBox)
+                    hideSoftKeyboard();
+                else {
+                    textBox = makeTextBox();
+                    layout.addView(textBox);
+                    layout.removeView(textButton);
+                    alreadyTextBox = true;
+                    textBox.requestFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.showSoftInput(textBox, InputMethodManager.SHOW_IMPLICIT);
+                }
+
+            }
+        });
+
+        /*imageView.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                if (!paintMode)
+                    return false;
+                else {
+                    float touchX = event.getX();
+                    float touchY = event.getY();
+                    switch (event.getAction()) {
+                        case MotionEvent.ACTION_DOWN:
+                            drawPath.moveTo(touchX, touchY);
+                            break;
+                        case MotionEvent.ACTION_MOVE:
+                            drawPath.lineTo(touchX, touchY);
+                            break;
+                        case MotionEvent.ACTION_UP:
+                            paintCanvas.drawPath(drawPath, paint);
+                            drawPath.reset();
+                            break;
+                        default:
+                            return false;
+                    }
+                    imageView.invalidate();
+                    return true;
+                }
+            }
+        });*/
+
+        //set behavior for close button
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                //go back to camera
+                Intent intent = new Intent(getApplicationContext(), CameraActivity.class);
+                startActivity(intent);
+            }
+        });
+
         // set behavior for save button
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
                 // turn off button so you cant upload twice
                 button.setClickable(false);
+                if (alreadyTextBox) {
+                    textBox.setCursorVisible(false);
+                    //textBox.setRotation(270);
+                    textBox.buildDrawingCache();
+                    //Matrix matrix = new Matrix();
+                    //matrix.postRotate(270);
+                    textBitmap = Bitmap.createBitmap(textBox.getDrawingCache());
+                    bitmap = addTextToPhoto(bitmap, textBitmap);
+                }
                 uploadPhoto(photo);
             }
         });
 
+        textButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (alreadyTextBox)
+                    return;
+                else {
+                    textBox = makeTextBox();
+                    layout.addView(textBox);
+                    layout.removeView(textButton);
+                    alreadyTextBox = true;
+                    textBox.requestFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.showSoftInput(textBox, InputMethodManager.SHOW_IMPLICIT);
+                }
+            }
+        });
+
+        paintButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //drawView = makeDrawingView();
+                //drawView.makeBitmap(bitmap);
+               /* if (!paintMode) {
+                    drawPath = new Path();
+                    paint = new Paint();
+                    paint.setColor(Color.RED);
+                    paint.setAntiAlias(true);
+                    paint.setStrokeWidth(20);
+                    paint.setStyle(Paint.Style.STROKE);
+                    paint.setStrokeJoin(Paint.Join.ROUND);
+                    paint.setStrokeCap(Paint.Cap.ROUND);
+                    canvasPaint = new Paint(Paint.DITHER_FLAG);
+                    paintBitmap = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+                    paintCanvas = new Canvas(paintBitmap);
+                    paintCanvas.drawBitmap(paintBitmap, 0, 0, canvasPaint);
+                    paintCanvas.drawPath(drawPath, paint);
+                    paintMode = true;
+                }*/
+            }
+        });
+
+    }
+
+    private void hideSoftKeyboard() {
+        InputMethodManager inputMethodManager = (InputMethodManager)  this.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(this.getCurrentFocus().getWindowToken(), 0);
+    }
+
+    private EditText makeTextBox() {
+        EditText editText = new EditText(this);
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.addRule(RelativeLayout.BELOW, paintButton.getId());
+        editText.setLayoutParams(params);
+        editText.setTextColor(Color.WHITE);
+        editText.setBackgroundColor(Color.TRANSPARENT);
+        return editText;
     }
 
     // Upload photo object and file to remote server
@@ -133,6 +271,16 @@ public class PhotoReviewActivity extends AppCompatActivity {
 
         // Assemble request to upload photo FILE
         File file = new File(this.getFilesDir() + "/temp_photo");
+        if (alreadyTextBox) {
+            try {
+                FileOutputStream fos = new FileOutputStream(file);
+                combined.compress(Bitmap.CompressFormat.PNG, 85, fos);
+                fos.flush();
+                fos.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
         PhotoClient service = ServiceGenerator.createService(PhotoClient.class);
         RequestBody requestFile = RequestBody.create(MediaType.parse("multipart/form-data"), file);
         RequestBody fileName = RequestBody.create(MediaType.parse("multipart/form-data"), photo.getFileName());
@@ -194,6 +342,26 @@ public class PhotoReviewActivity extends AppCompatActivity {
                 Log.d("Error", t.getMessage());
             }
         });
+    }
+
+    public Bitmap addTextToPhoto(Bitmap bitmap1, Bitmap bitmap2) {
+        //Matrix matrix = new Matrix();
+        //matrix.setRotate(90, bitmap2.getWidth()/2, bitmap2.getHeight()/2);
+        //Bitmap rotatedBitmap = Bitmap.createBitmap(bitmap2, 0, 0, bitmap2.getWidth(), bitmap2.getHeight(), matrix, true);
+        //Bitmap rotatedBitmap = Bitmap.createBitmap(bitmap2.getWidth(), bitmap2.getHeight(), bitmap2.getConfig());
+        combined = Bitmap.createBitmap(bitmap1.getWidth(), bitmap1.getHeight(), bitmap1.getConfig());
+        canvas = new Canvas(combined);
+        canvas.drawBitmap(bitmap1, new Matrix(), null);
+        canvas.drawBitmap(bitmap2, 0, 0, new Paint());
+
+        /*Bitmap targetBitmap = Bitmap.createBitmap(targetWidth, targetHeight, config);
+        Canvas canvas = new Canvas(targetBitmap);
+        Matrix matrix = new Matrix();
+        matrix.setRotate(mRotation,source.getWidth()/2,source.getHeight()/2);
+        canvas.drawBitmap(source, matrix, new Paint());*/
+
+
+        return combined;
     }
 
     // rotate a bitmap in order to save image as proper orientation
